7c94eb9e369dff5309de9933ed4c81636108d74b
adi_walture <adityawalture9298@gmail.com>
2023-11-23 22:54:23 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,19 @@ const app = express();
 const taskRoute = require("./controller/taskRoute")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-mongoose.set("strictQuery", true);
-mongoose.connect(
-mongodburl= "mongodb+srv://mayank123:mayank123@cluster0.hfooxct.mongodb.net/taskdb"
-);
-var db = mongoose.connection;
-db.on("open", () => console.log("Connected to Db"));
-db.on("error", () => console.log("Not Connected to Db"));
+// mongoose.set("strictQuery", true);0
+require("dotenv").config();
+// mongoose.connect(
+// // mongodburl= "mongodb+srv://mayank123:mayank123@cluster0.hfooxct.mongodb.net/taskdb"
+// );
+// var db = mongoose.connection;
+// db.on("open", () => console.log("Connected to Db"));
+// db.on("error", () => console.log("Not Connected to Db"));
+
+mongoose
+  .connect(process.env.mongodburl)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
